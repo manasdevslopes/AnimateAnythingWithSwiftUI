4c7353276bdf19c0e16c412d4ec23e1758c4de8d
Manas Vijaywargiya <manasdevslopes@gmail.com>
2021-07-20 21:26:32 +0530

    Initial Commit

---
+++ JustAnimateSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JustAnimateSwiftUI
+//
+//  Created by MANAS VIJAYWARGIYA on 20/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JustAnimateSwiftUI/JustAnimateSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustAnimateSwiftUIApp.swift
+//  JustAnimateSwiftUI
+//
+//  Created by MANAS VIJAYWARGIYA on 20/07/21.
+//
+
+import SwiftUI
+
+@main
+struct JustAnimateSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
